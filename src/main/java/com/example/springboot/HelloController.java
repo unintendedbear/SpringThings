c70dc78b9207f480a8ec6215abd1377129d52054
unintendedbear <unintendedbear@gmail.com>
2021-03-28 13:34:45 +0200

    Hello World!

---
+++ src/main/java/com/example/springboot/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.springboot;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/*
+The class is flagged as a @RestController, meaning it is ready for use by Spring MVC to handle web requests.
+@RequestMapping maps / to the index() method.
+When invoked from a browser or by using curl on the command line, the method returns pure text.
+That is because @RestController combines @Controller and @ResponseBody, two annotations that results in web requests returning data rather than a view. 
+*/
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Hello World!";
+	}
+
+}
